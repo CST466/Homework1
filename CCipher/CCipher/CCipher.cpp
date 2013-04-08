372dbd5794210c483e8f74456b189b5ab088f999
Mike <linuxdude9@gmail.com>
2013-04-08 16:55:56 -0700

    Implement Encrypt and Decrypt

--- CCipher/CCipher/CCipher.cpp
+++ CCipher/CCipher/CCipher.cpp
@@ -14,3 +14,29 @@ int main(int argc, char *argv[])
 	if(argc != 4)
 		cout << "usage: " << argv[0] << "
 }
+
+
+
+void Encrypt(unsigned char * plain_text, int key)
+{
+	int length = strlen(plain_text);
+	const unsigned char ASCII_END = 0x7A;
+	// Normalize key
+	key = key % 26;
+	for(int i = 0; i < length; ++i)
+	{
+		plain_text[i] = (plain_text[i] + (unsigned char)key ) % ASCII_END;
+	}
+}
+
+void Decrypt(unsigned char * plain_text, int key)
+{
+	int length = strlen(plain_text);
+	const unsigned char ASCII_END = 0x7A;
+	key = key % 26;
+	
+	for(int i = 0; i < length; ++i)
+	{
+		plain_text[i] = (plain_text[i] + -key) % ASCII_END;
+	}
+}

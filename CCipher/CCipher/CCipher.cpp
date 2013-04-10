2a05c070b01d56e4fdae88da36b726aea14d3748
MechAlucard <tyler.j.martin@gmail.com>
2013-04-10 10:34:11 -0700

    Added function to tell if file opened was a plain text file

--- CCipher/CCipher/CCipher.cpp
+++ CCipher/CCipher/CCipher.cpp
@@ -3,6 +3,7 @@
 #include <string>
 using std::string;
 using std::cout;
+using std::ifstream;
 // USAGE:  this program encrypts or decrypts a text file given a key and source file
 // CCipher <option> <key> <source.txt>
 // option
@@ -15,6 +16,8 @@ using std::cout;
 //	file to encrypt/decrypt
 void Decrypt(char * plain_text, int key);
 void Encrypt(char * plain_text, int key);
+std::ifstream::pos_type filesize(const char* filename);
+bool isPlainText(string file_name);
 int main(int argc, char *argv[])
 {
 	char tmpStr[3000];
@@ -73,8 +76,16 @@ int main(int argc, char *argv[])
 		std::ifstream TextFile(argv[3]);
 		if(TextFile.is_open())
 		{
-			TextFile >> tmpStr;
-			haveFile = true;
+			if(isPlainText(argv[3]))
+			{
+				TextFile >> tmpStr;
+				haveFile = true;
+			}
+			else
+			{
+				cout << "NOT A TEXT FILE!!";
+				haveFile = false;
+			}
 		}
 		else
 		{
@@ -150,4 +161,35 @@ void Decrypt(char * encrypted_text, int key)
 		encrypted_text[i] = (encrypted_text[i] - 'A' - key) % 26 + 'a';
 		encrypted_text[i] = encrypted_text[i] < 'a' ? encrypted_text[i] + 26 : encrypted_text[i];
 	}
+}
+
+bool isPlainText(string file_name)
+{
+	size_t file_size = filesize(file_name.c_str());
+	char * file_contents = new char[file_size];
+	
+	ifstream in_file;
+	in_file.open( file_name.c_str(), std::ios::binary | std::ios::in);
+	in_file.read(file_contents, file_size);
+	in_file.close();
+	
+	bool is_plain_text = true;
+	
+	for(int i = 0; i < file_size && is_plain_text; ++i)
+	{
+		// Conditions for file data not being ascii text
+		// from http://stackoverflow.com/questions/277521/how-to-identify-the-file-content-as-ascii-or-binary
+		if( (file_contents[i] < 9) || (file_contents[i] > 13 && file_contents[i] < 32) || (file_contents[i] > 126))
+		{
+			is_plain_text = false;	
+		}
+	}
+	delete file_contents;
+	return is_plain_text;
+}
+std::ifstream::pos_type filesize(const char* filename)
+{
+    std::ifstream in(filename, std::ifstream::in | std::ifstream::binary);
+    in.seekg(0, std::ifstream::end);
+    return in.tellg(); 
 }
